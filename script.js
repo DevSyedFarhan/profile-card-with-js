37b519d2d57bb20877fe9393b03c89716b547635
unknown <syedfarhangraphic@gmail.com>
2024-12-29 06:02:40 +0500

    toggle and status updation feature added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //card infromation
 let arr=[
-    {name : "John Smith", img : "https://images.unsplash.com/photo-1631588310958-dec1630f8df5?q=80&w=1527&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" , status :"Freind"},
+    {name : "John Smith", img : "https://images.unsplash.com/photo-1631588310958-dec1630f8df5?q=80&w=1527&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" , status :"Stranger"},
     {name : "Julia Bennete", img : "https://plus.unsplash.com/premium_photo-1681494639261-7908ef9d2257?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" , status :"Stranger"},
-    {name : "Mark Adam", img : "https://images.unsplash.com/photo-1624835567150-0c530a20d8cc?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" , status :"Influencer"},
+    {name : "Mark Adam", img : "https://images.unsplash.com/photo-1624835567150-0c530a20d8cc?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" , status :"Stranger"},
 ];
 function print(){
     let clutter = ""
@@ -13,9 +13,9 @@ arr.forEach(function(val, index){
                 <img src= "${val.img}"></img>
             </div>
             <h3>${val.name}</h3>
-            <h5>${val.status}</h5>
+            <h5 id="${val.status}">${val.status}</h5>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo quia ea nulla, nihil iure saepe!</p>
-            <button id="${index}" class ="follow-btn">Follow</button>
+            <button id="${index}" class ="${val.status === "Stranger" ? "Blue" : "Red"}">${val.status === "Stranger" ? "Follow" : "Unfollow"}</button>
         </div>`;
 })
 
@@ -24,25 +24,16 @@ document.querySelector("#main").innerHTML = clutter;
 
 print();
 
-document.querySelector("#main");
-addEventListener("click", function(details){
-    arr[details.target.id].status = "freinds";
-    print();
+document.querySelector("#main").addEventListener("click", function(details){
+    if(details.target.tagName === "BUTTON"){
+        const index = details.target.id;
+        if(arr[index].status === "Stranger"){
+            arr[index].status = "Friend";
+        }else{
+            arr[index].status = "Stranger";
+        }
+        print();
+    }
 })
 
-
-
-// let buttons = document.querySelectorAll(".follow-btn");
-// //follow & following function
-// buttons.forEach(function(button){
-//     button.addEventListener("click", function(){
-//         if(this.textContent === "Follow"){
-//             this.textContent = "Following";
-//             this.style.backgroundColor = "Red";
-//         }else{
-//             this.textContent = "Follow";
-//             this.style.backgroundColor = "";
-            
-//         }
-//     })
-// });
+print();
